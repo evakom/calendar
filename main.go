ec4183b5aa20589ca5644be5855a3821d5cdb8b8
Eugene Klimov <tirava@gmail.com>
2019-10-23 22:27:08 +0300

    Calendar events draft

--- main.go
+++ main.go
@@ -8,6 +8,31 @@
 
 package main
 
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"log"
+)
+
 func main() {
 
+	events := &Events{}
+	events.addEvent(newEvent())
+
+	out, err := proto.Marshal(events)
+	if err != nil {
+		log.Fatalln("Failed to encode event:", err)
+	}
+
+	events1 := &Events{}
+	if err := proto.Unmarshal(out, events1); err != nil {
+		log.Fatalln("Failed to parse event:", err)
+	}
+
+	event := newEvent()
+	event.Subject = "222222222222222222222"
+	event.Body = "3333333333333333333"
+	events1.addEvent(event)
+
+	fmt.Println(events1.GetEvents())
 }
