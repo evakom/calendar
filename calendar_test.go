c835be253f342472c015a602118829ed68d6712c
Eugene Klimov <tirava@gmail.com>
2019-10-24 18:43:15 +0300

    Calendar more tests

--- calendar_test.go
+++ calendar_test.go
@@ -6,7 +6,12 @@
 
 package calendar
 
-import "testing"
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"reflect"
+	"testing"
+)
 
 func TestNewEvent(t *testing.T) {
 	e1 := newEvent().GetId()
@@ -17,8 +22,64 @@ func TestNewEvent(t *testing.T) {
 }
 
 func TestNewDB(t *testing.T) {
+	if _, err := createNewDB(); err != nil {
+		t.Fatalf("Can't create new DB, error: %v", err)
+	}
+}
+
+func TestProto(t *testing.T) {
+	e1 := &Event{}
+	e1.Subject = "testik"
+	out, err := proto.Marshal(e1)
+	if err != nil {
+		t.Fatalf("Failed to encode event: %v, error: %v", e1, err)
+	}
+	e2 := &Event{}
+	err = proto.Unmarshal(out, e2)
+	if err != nil {
+		t.Fatalf("Failed to parse event: %v, error: %v", out, err)
+	}
+	e2.XXX_sizecache = 8 // Subject length + 1
+	if !reflect.DeepEqual(e1, e2) {
+		t.Errorf("Event1 not equal Event2 after Marshal/Unmarshal:\n%#v\n%#v", e1, e2)
+	}
+}
+
+func TestAddEvent(t *testing.T) {
+	events, _ := createNewDB()
+	e := newEvent()
+	e.Subject = "222222222222222222222"
+	e.Body = "3333333333333333333"
+	_ = events.addEvent(e)
+	e = newEvent()
+	e.Duration = 555
+	_ = events.addEvent(e)
+	l := len(events.events)
+	if l != 2 {
+		t.Errorf("After adding 2 events to MapDB length != 2, actual length = %d", l)
+	}
+}
+
+func TestEditEvent(t *testing.T) {
+
+}
+
+func TestDelEvent(t *testing.T) {
+
+}
+
+func TestGetEvent(t *testing.T) {
+
+}
+
+func TestGetAllEvents(t *testing.T) {
+
+}
+
+func createNewDB() (*dbMapEvents, error) {
 	events := newDB(&dbMapEvents{})
 	if _, ok := events.(*dbMapEvents); !ok {
-		t.Errorf("Can't cast to *dbMapEvents type: %+v", events)
+		return nil, fmt.Errorf("error while cast events to *dbMapEvents:\n%#v", events)
 	}
+	return events.(*dbMapEvents), nil
 }
